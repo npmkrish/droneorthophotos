5da3649b0bc907d27c77f860e8e2182a19f83ed7
Krish Kapoor <kapoorkrish191@gmail.com>
2026-05-08 11:18:47 +0530

    predicted.py

--- predicted (1).py
+++ predicted (1).py
@@ -12,6 +12,7 @@ if directories:
     last_directory = directories[-1]
     last_directory_path = os.path.join(folder_path, last_directory)
     last_directory_path = os.path.normpath(last_directory_path)  # Normalize the path
+    last_directory_path = os.path.abspath(last_directory_path)  # Get absolute path
 
     # List all file paths in the last directory
     file_paths = [os.path.join(last_directory_path, file) for file in os.listdir(last_directory_path) if os.path.isfile(os.path.join(last_directory_path, file))]
