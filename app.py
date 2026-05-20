336dc76749adeb490c793f2283caa71de4b12c2f
Krish Kapoor <kapoorkrish191@gmail.com>
2026-05-20 22:21:05 +0530

    app.py

--- app.py
+++ app.py
@@ -699,6 +699,9 @@ def method_not_allowed(e):
         return render_template('405.html', user_name=user_name)
     # return render_template('405.html'), 405
     return redirect(url_for('405.html',user_name="MICHAEL JACKSON"))
+def method_not_allowed(e):
+    if 'email' in session:
+        
 
 
 
