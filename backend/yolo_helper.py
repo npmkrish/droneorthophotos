8aac1c0abccbbd447f077427d8e214e8de3a9b40
Krish Kapoor <kapoorkrish191@gmail.com>
2026-06-09 22:38:15 +0530

    add dependencies

--- backend/yolo_helper.py
+++ backend/yolo_helper.py
@@ -155,11 +155,4 @@ def main():
 
 if __name__ == '__main__':
     main()
-elif __name__ == '__main__':
-    main()    
-elif sys.argv[0].endswith('yolo_helper.py'):
-    main()   
-elif os.path.basename(sys.argv[0]) == 'yolo_helper.py':
-    main()   
-else:
-    pass      
+   
